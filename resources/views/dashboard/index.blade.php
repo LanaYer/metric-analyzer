dc1e5b52720d222558681fc9d3834393142b7ecb
LanaYer <yermolaeva.lana@gmail.com>
2018-01-16 22:42:23 +0300

    models

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,11 @@
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    13q53q535
+                </div>
+            </div>
+        </div>
+    </div>
+@show
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                   @if (Auth::guest())
+                        @include("auth.login")
+                   @else
+                         <div class="panel-heading">Dashboard</div>
 
-                    You are logged in!
+                         <div class="panel-body">
+                             @include("dashboard.index")
+                         </div>
+                   @endif
                 </div>
             </div>
         </div>
